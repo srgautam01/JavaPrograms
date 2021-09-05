21001c225a14a546c9fd252ada8049e6dcde95ed
sundar <sundar.gautam@selu.edu>
2021-09-05 16:42:39 -0500

    java program to find largest two numbers in a given array.

--- src/Simple_Interest.java
+++ src/Simple_Interest.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Simple_Interest {
+import java.util.Scanner;
+
+public class Simple_Interest {
+    public static void main(String[] args){
+        // this java program finds the simple interest.
+        float p,r,t;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the principal:");
+        p = sc.nextFloat();
+        System.out.println("Enter the rate of interest:");
+        r =sc.nextFloat();
+        System.out.println("Enter the time period:");
+        t = sc.nextFloat();
+
+        float si;
+        si = (r*t*p) / 100;
+        System.out.print("The simple interest is:" + si);
+    }
 }
