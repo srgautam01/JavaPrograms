8f149a20a49d92954e1dfe692b0208f4b298d9da
sundar <sundar.gautam@selu.edu>
2021-09-06 10:14:47 -0500

    bitwise operations

--- src/Multiply_Bitwise.java
+++ src/Multiply_Bitwise.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Multiply_Bitwise {
+import java.util.Scanner;
+
+public class Multiply_Bitwise {
+    public static void main(String[] args){
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        n = sc.nextInt();
+        int mul = n << 2;
+        System.out.println("Answer:"+mul);
+    }
 }
