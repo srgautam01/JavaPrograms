4595deb03321be1063f72b193fe4e9ba9adcc5d0
sundar <sundar.gautam@selu.edu>
2021-09-27 13:25:55 -0500

    name instance variable.

---
+++ src/Account.java
@@ -0,0 +1,18 @@
+// fig 3.1 Account.java
+// Account class that contains a name instance variable
+// and methods to set and get its value
+
+public class Account {
+    private String name;  // instance variable
+
+    // method to set the name in the object
+    public void setName(String name)
+    {
+        this.name = name;  // store the name
+    }
+    // method to retrieve the name from the object
+    public String getName()
+    {
+        return name;  // return the value of name to caller
+    }
+}    // end class Account
