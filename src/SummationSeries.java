0fe02b2c2cefefdc64a5ddf72166454ed332fb89
sundar <sundar.gautam@selu.edu>
2021-09-29 01:24:11 -0500

    summation of series.

---
+++ src/SummationSeries.java
@@ -0,0 +1,16 @@
+/**
+ * how to program in java.
+ *      (Summation of a series) Write a program that displays the result of
+ *￼         1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9
+ *
+ * Solved by sundar raj gautam 09/29/2021
+ */
+
+
+public class SummationSeries {
+    public static void main(String[] args){
+        int answer = 1+2+3+4+5+6+7+8+9;
+
+        System.out.println("1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9= " + answer);
+    }
+}
