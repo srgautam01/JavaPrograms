e086f676fd313ac99294828b043ea2c5ac92b377
sundar <sundar.gautam@selu.edu>
2021-09-29 14:24:00 -0500

    loop to print numbers from 1 to 10.

---
+++ src/Loop101.java
@@ -0,0 +1,9 @@
+// this program print the numbers from 1 to 19 in seperate lines
+
+public class Loop101 {  // main class
+    public static void main(String[] args){  // main method begins execution of java application
+        for(int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
