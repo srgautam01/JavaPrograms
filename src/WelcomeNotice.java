117d77d9a804076b63f2a329ab972e215284b8b2
sundar <sundar.gautam@selu.edu>
2021-09-29 01:08:36 -0500

    Welcome notice.

---
+++ src/WelcomeNotice.java
@@ -0,0 +1,12 @@
+/*
+* Write a program that displays welcome to java five times
+*
+* */
+
+public class WelcomeNotice {
+    public static void main(String[] args){
+        for(int i=0; i<5; i++){
+            System.out.println("Welcome to java!");
+        }
+    }
+}
