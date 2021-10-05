2aca53e0f810b37020bfb1173515c2bde1ada58b
sundar <sundar.gautam@selu.edu>
2021-10-05 00:18:27 -0500

    account constructor to initialize the name instance variable at the time each account object is created.

---
+++ src/AccountTest102.java
@@ -0,0 +1,17 @@
+/*
+* AccountTest102.java
+* Using the account constructor to initialize the name instance
+* variable at the time each account object is created
+* */
+
+//public class AccountTest102 {
+//    public static void main(String[] args){
+//        // create two account objects
+//        Account account1 = new Account("Jane Green");
+//        Account account2 = new Account("John Blue");
+//
+//        // display initial value of name for each account
+//        System.out.printf("account1 name is: %s%n", account1.getName());
+//        System.out.printf("account2 name is: %s%n", account2.getName());
+//    }
+//}// end class AccountTest102
